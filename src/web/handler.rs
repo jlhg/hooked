15bbf5f43510a488889c84e78f6db9edc33cf396
Jian-Long Huang <contact@jlhg.org>
2023-10-23 18:02:19 +0800

    Fix branch value

--- src/web/handler.rs
+++ src/web/handler.rs
@@ -63,7 +63,7 @@ pub async fn post_webhooks_github(
                         return render_bad_request("invalid [ref] value in the payload");
                     }
 
-                    let branch = git_ref.strip_prefix("\"refs/heads/").unwrap_or("null");
+                    let branch = git_ref.trim_matches('"').strip_prefix("refs/heads/").unwrap_or("null");
                     if branch == "null" {
                         return render_bad_request("invalid stripped [ref] value in the payload");
                     }
